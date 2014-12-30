2cad9603ccba322caf1fbb65704792c90a2fdee4
nikos <nparasta@gmail.com>
2014-12-30 15:33:12 +0100

    Making form

--- guardian_news.admin.php
+++ guardian_news.admin.php
@@ -9,12 +9,13 @@
  * Implements hook_menu().
  */
 function guardian_news_menu() {
-  $items['admin/guardian_news'] = array(
+  $items['admin/content/guardian_news'] = array(
     'title' => 'The Guardian news',
+    'type' => MENU_NORMAL_ITEM,
     'description' => 'Admin page for module guardian_news.',
     'page callback' => 'drupal_get_form',
     'page arguments' => array('_guardian_news_admin_page'),
-    'access arguments' => array('access admin pages'),
+    'access arguments' => TRUE,
   );
 
   return $items;
@@ -23,11 +24,32 @@ function guardian_news_menu() {
 /**
  * Admin page for module
  */
-function _guardian_news_admin_page(&$form, $form_state) {
+function _guardian_news_admin_page($form, &$form_state) {
 
   $form['title'] = array(
-
+    '#type' => 'markup',
   );
 
+  $form['post_number'] = array(
+    '#type' => 'select',
+    '#options' => array(
+      1 => '1',
+      2 => '2',
+      3 => '3',
+      4 => '4',
+      5 => '5',
+      6 => '6',
+      7 => '7',
+      8 => '8',
+      9 => '9',
+      10 => '10',
+    ),
+  );
 
+  $form['submit_button'] = array(
+    '#type' => 'submit',
+    '#value' => t('Save changes'),
+  );
+
+  return $form;
 }
