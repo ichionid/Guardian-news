e1d6c12b2f4cc0e0775c096ccdf4ce34d14c72c9
np <np@peytz.dk>
2015-01-06 11:49:46 +0100

    Fixing template and nojs implementation

--- templates/guardian_news.tpl.php
+++ templates/guardian_news.tpl.php
@@ -3,18 +3,20 @@
  * @file
  * Template for The Guardian news module.
  */
+dpm($articles);
 ?>
 <div>
   <div>
 
+    <ul>
     <?php foreach ($articles as $article) : ?>
-    <li>
-      <a href=""><h3>{{article.webTitle}}</h3></a>
-      <p>{{article.webPublicationDate| date:"MM/dd/yyyy 'at' h:mma"}}</p>
-      <p>Temperature: {{article.sectionName}}</p>
-    </li>
+      <li>
+        <a href="<?php print $article['webUrl']; ?>"><h3><?php print $article['webTitle']; ?></h3></a>
+        <p><?php print $article['webPublicationDate']; ?> | <?php print $article['sectionName']; ?></p>
+      </li>
     <?php endforeach; ?>
+    </ul>
 
-    <button>More</button>
+    <div><a href="">More</a></div>
   </div>
 </div>
